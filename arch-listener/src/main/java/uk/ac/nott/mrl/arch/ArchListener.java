39ed4846dc2e6184717b2e69e4a9e7f86437c980
Kevin Glover <ktg@cs.nott.ac.uk>
2016-01-29 11:50:25 +0000

    Serial port

--- arch-listener/src/main/java/uk/ac/nott/mrl/arch/ArchListener.java
+++ arch-listener/src/main/java/uk/ac/nott/mrl/arch/ArchListener.java
@@ -1,102 +1,117 @@
 package uk.ac.nott.mrl.arch;
 
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import jssc.SerialPort;
+import jssc.SerialPortEvent;
+import jssc.SerialPortEventListener;
+import jssc.SerialPortException;
 import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class ArchListener implements Runnable
+public class ArchListener implements SerialPortEventListener
 {
-	public static void main(String[] args)
-	{
-		ArchListener archListener = new ArchListener(901);
-		new Thread(archListener).start();
-	}
-
 	private static final Logger logger = Logger.getLogger("");
-
-	private final int port;
-	private transient boolean running = true;
+	private final SerialPort serialPort;
 	private final OkHttpClient httpClient;
 
-	public ArchListener(int port)
+	public ArchListener(String port)
 	{
-		this.port = port;
+		serialPort = new SerialPort("COM1");
+		try
+		{
+			serialPort.openPort();//Open port
+			serialPort.setParams(9600, 8, 1, 0);//Set params
+			int mask = SerialPort.MASK_RXCHAR + SerialPort.MASK_CTS + SerialPort.MASK_DSR;//Prepare mask
+			serialPort.setEventsMask(mask);//Set mask
+			serialPort.addEventListener(this);//Add SerialPortEventListener
+		}
+		catch (SerialPortException e)
+		{
+			logger.log(Level.WARNING, e.getMessage(), e);
+		}
+
 		OkHttpClient.Builder builder = new OkHttpClient.Builder();
 		//builder.cache(new Cache(getCacheDir(), cacheSize));
 		httpClient = builder.build();
 	}
 
-	@Override
-	public synchronized void run()
+	public static void main(String[] args)
 	{
-		boolean error = false;
-		while (running)
+		ArchListener archListener = new ArchListener("COM1");
+	}
+
+	StringBuilder message = new StringBuilder();
+
+	public void serialEvent(SerialPortEvent event)
+	{
+		if (event.isRXCHAR() && event.getEventValue() > 0)
 		{
 			try
 			{
-				if (error)
+				byte buffer[] = serialPort.readBytes();
+				for (byte b : buffer)
 				{
-					wait(1000);
-				}
-
-				final DatagramSocket serverSocket = new DatagramSocket(port);
-				logger.info("Listening for input on port " + port);
-				final byte[] receivedData = new byte[1024];
-				while (running)
-				{
-					try
+					if ((b == '\r' || b == '\n') && message.length() > 0)
 					{
-						final DatagramPacket receivePacket = new DatagramPacket(receivedData, receivedData.length);
-						serverSocket.receive(receivePacket);
-
-						String string = new String(receivedData, 0, receivePacket.getLength());
-						string = string.toLowerCase();
-						String[] parts = string.split(" ");
-
-						logger.info(string);
-						if (parts.length >= 2)
-						{
-							final FormBody.Builder bodyBuilder = new FormBody.Builder()
-									.add("state", parts[1]);
-							if(parts[0].equals("stateleft"))
-							{
-								bodyBuilder.add("direction", "left");
-							}
-							else if(parts[0].equals("stateright"))
-							{
-								bodyBuilder.add("direction", "right");
-							}
-
-							if (parts.length >= 3)
-							{
-								bodyBuilder.add("height", parts[3]);
-							}
-
-
-							final Request.Builder builder = new Request.Builder()
-									.post(bodyBuilder.build())
-									.url("http://localhost/state");
-							final Request request = builder.build();
-							final Response response = httpClient.newCall(request).execute();
-						}
-
+						String toProcess = message.toString();
+						processMessage(toProcess);
+						message.setLength(0);
 					}
-					catch (Throwable e)
+					else
 					{
-						logger.log(Level.SEVERE, e.getMessage(), e);
+						message.append((char) b);
 					}
 				}
 			}
-			catch (Throwable e)
+			catch (SerialPortException e)
 			{
-				logger.log(Level.SEVERE, e.getMessage(), e);
-				error = true;
+				logger.log(Level.WARNING, e.getMessage(), e);
+			}
+		}
+	}
+
+	private void processMessage(String message)
+	{
+		String string = message.toLowerCase();
+		if(string.startsWith("state"))
+		{
+			String[] parts = string.split(" ");
+
+			logger.info(string);
+			if (parts.length >= 2)
+			{
+				final FormBody.Builder bodyBuilder = new FormBody.Builder()
+						.add("state", parts[1]);
+				if (parts[0].equals("stateleft"))
+				{
+					bodyBuilder.add("direction", "left");
+				}
+				else if (parts[0].equals("stateright"))
+				{
+					bodyBuilder.add("direction", "right");
+				}
+
+				if (parts.length >= 3)
+				{
+					bodyBuilder.add("height", parts[3]);
+				}
+
+				final Request.Builder builder = new Request.Builder()
+						.post(bodyBuilder.build())
+						.url("http://localhost/state");
+				final Request request = builder.build();
+				try
+				{
+					final Response response = httpClient.newCall(request).execute();
+				}
+				catch (Exception e)
+				{
+					logger.log(Level.WARNING, e.getMessage(), e);
+				}
 			}
 		}
 	}
