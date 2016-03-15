e37ca06b2a1d7c1c1de07067476c2ed544f77b0b
Kevin Glover <ktg@cs.nott.ac.uk>
2016-03-15 13:33:15 +0000

    Update copyright

---
+++ arch-server/build/exploded-app/js/item.js
@@ -0,0 +1,113 @@
+(function poll() {
+    $.ajax({
+        url: "/item",
+        type: "GET",
+        ifModified: true,
+        success: function(item) {
+            //console.log(data);
+            if(item != null)
+            {
+                for (var prop in item)
+                {
+                    $('#'+prop).text(item[prop]);
+                }
+
+                if(item.author == null)
+                {
+                    $('#author').text('');
+                }
+
+				if(item.data != null)
+				{
+					$('#data').empty()
+					for (var i = 0; i < item.data.length; i++)
+					{
+						$('#data').append('<div>').append(item.data[i]).append('</div>');
+                    }
+				}
+
+				if(item.height != null)
+				{
+					$('#data').append('<div>').append(item.height).append('</div>');
+				}
+
+				if(item.state == "waiting" || item.state == "left")
+				{
+					$('#circle').css('opacity', '1');
+				}
+
+                if(item.state == "engagement")
+                {
+                    $('#circle').css('opacity', '0');
+                }
+
+				if(window.location.pathname.endsWith(item.direction +".html"))
+				{
+					if(item.state == "engagement")
+					{
+	                    $('#approach').css('opacity', '1');
+				        $('#leave').css('opacity', '0');
+	                    $('#author').css('opacity', '0');
+	                    $('#data').css('opacity', '0');
+					}
+					else
+					{
+	                    $('#approach').css('opacity', '0');
+			            $('#leave').css('opacity', '0');
+	                    $('#author').css('opacity', '0');
+	                    $('#data').css('opacity', '0');
+					}
+				}
+				else if(item.state == "approaching")
+				{
+                   	$('#approach').css('opacity', '0');
+                    $('#leave').css('opacity', '1');
+                    $('#author').css('opacity', '1');
+                    $('#data').css('opacity', '0');
+                    $('#circle').css('opacity', '0');
+				}
+				else if(item.state == "under" || item.state == "leaving")
+                {
+                   	$('#approach').css('opacity', '0');
+                    $('#leave').css('opacity', '1');
+                    $('#author').css('opacity', '1');
+                    $('#data').css('opacity', '1');
+                    $('#circle').css('opacity', '0');
+                }
+                else
+                {
+                	$('#approach').css('opacity', '0');
+                    $('#leave').css('opacity', '0');
+                    $('#author').css('opacity', '0');
+                	$('#data').css('opacity', '0');
+				}
+			}
+        },
+        dataType: "json",
+        complete: setTimeout(function() {poll()}, 100),
+        timeout: 200
+    });
+})();
+
+$(document).keypress(function(e)
+{
+    switch(e.which) {
+        case 122:
+            $('#approach').css('opacity', '0');
+            $('#leave').css('opacity', '0');
+            $('#data').css('opacity', '0');
+            break;
+
+        case 120:
+            $('#approach').css('opacity', '1');
+            $('#leave').css('opacity', '0');
+            $('#data').css('opacity', '0');
+            break;
+
+        case 99:
+            $('#approach').css('opacity', '0');
+            $('#leave').css('opacity', '1');
+            $('#data').css('opacity', '1');
+            break;
+        }
+    });
--- arch-server/src/main/java/uk/ac/nott/mrl/arch/server/StateServlet.java
+++ arch-server/src/main/java/uk/ac/nott/mrl/arch/server/StateServlet.java
@@ -1,4 +1,22 @@
 /*
+ *     Light Night Arch Control software
+ *     Copyright (C) 2016 University of Nottingham
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as
+ *     published by the Free Software Foundation, either version 3 of the
+ *     License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
    For step-by-step instructions on connecting your Android application to this backend module,
    see "App Engine Java Servlet Module" template documentation at
    https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloWorld
